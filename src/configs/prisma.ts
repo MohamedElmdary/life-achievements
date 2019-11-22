027d2cd21d4f13e6a67cc2867f88f7ab8c4fe7c4
MohamedElmdary <engm5081@gmail.com>
2019-11-22 20:58:21 +0200

    added end_point env to prisma

--- src/configs/prisma.ts
+++ src/configs/prisma.ts
@@ -1,10 +1,10 @@
 import { Prisma } from 'prisma-binding';
 
 const prisma = new Prisma({
-  debug: !!process.env.DEVELOPMENT,
-  secret: process.env.PRISMA_SECRET,
-  typeDefs: 'generated/schema.prisma.graphql',
-  endpoint: 'http://localhost:4466/'
+    debug: !!process.env.DEVELOPMENT,
+    secret: process.env.PRISMA_SECRET,
+    typeDefs: 'generated/schema.prisma.graphql',
+    endpoint: process.env.END_POINT
 });
 
 export { prisma };
