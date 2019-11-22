9e8ae2082c4dc0f189e44ded3a2dc237eb6db52b
MohamedElmdary <engm5081@gmail.com>
2019-11-22 21:11:18 +0200

    change playground url

--- src/configs/yoga.ts
+++ src/configs/yoga.ts
@@ -1,26 +1,26 @@
 import { Options } from 'graphql-yoga';
 
 const options: Options = {
-  debug: !!process.env.DEVELOPMENT,
-  playground: !!process.env.DEVELOPMENT ? '/playground' : undefined,
-  port: process.env.PORT,
-  formatError({ originalError }: any) {
-    return originalError.message;
-  },
-  formatResponse(r: any) {
-    if (r.errors && !r.data) {
-      return {
-        data: null,
-        success: false,
-        errors: [].concat.apply([], r.errors)
-      };
+    debug: !!process.env.DEVELOPMENT,
+    playground: !!process.env.DEVELOPMENT ? '/' : undefined,
+    port: process.env.PORT,
+    formatError({ originalError }: any) {
+        return originalError.message;
+    },
+    formatResponse(r: any) {
+        if (r.errors && !r.data) {
+            return {
+                data: null,
+                success: false,
+                errors: [].concat.apply([], r.errors)
+            };
+        }
+        return {
+            ...r,
+            success: true,
+            errors: null
+        };
     }
-    return {
-      ...r,
-      success: true,
-      errors: null
-    };
-  }
 };
 
 export { options };
