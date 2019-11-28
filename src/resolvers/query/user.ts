fd11ad61e53786ec021fce1c68c7db94f54d4360
MohamedElmdary <engm5081@gmail.com>
2019-11-28 19:43:53 +0200

    updated typescript & fixed issue in searchUsers query

--- src/resolvers/query/user.ts
+++ src/resolvers/query/user.ts
@@ -2,61 +2,62 @@ import { Resolver } from '../resolver.interface';
 import { User } from '@generated';
 
 const searchUsers: Resolver<{ name: string }, User[]> = async (
-  _,
-  { data: { name } },
-  { query, req },
-  info
-) => {
-  return await query.users(
-    {
-      where: {
-        register_code: null,
-        id_not: (<any>req).userId,
-        OR: [
-          {
-            first_name_contains: name
-          },
-          {
-            first_name_contains: name.toLocaleLowerCase()
-          },
-          {
-            first_name_contains:
-              name[0].toLocaleUpperCase() + name.slice(1).toLocaleLowerCase()
-          },
-          {
-            last_name_contains: name
-          },
-          {
-            last_name_contains: name.toLocaleLowerCase()
-          },
-          {
-            last_name_contains:
-              name[0].toLocaleUpperCase() + name.slice(1).toLocaleLowerCase()
-          }
-        ]
-      }
-    },
+    _,
+    { data: { name } },
+    { query, req },
     info
-  );
+) => {
+    const formatedName =
+        name[0]?.toLocaleUpperCase() ?? '' + name.slice(1).toLocaleLowerCase();
+
+    return await query.users(
+        {
+            where: {
+                register_code: null,
+                id_not: (<any>req).userId,
+                OR: [
+                    {
+                        first_name_contains: name
+                    },
+                    {
+                        first_name_contains: name.toLocaleLowerCase()
+                    },
+                    {
+                        first_name_contains: formatedName
+                    },
+                    {
+                        last_name_contains: name
+                    },
+                    {
+                        last_name_contains: name.toLocaleLowerCase()
+                    },
+                    {
+                        last_name_contains: formatedName
+                    }
+                ]
+            }
+        },
+        info
+    );
 };
 
 const getUser: Resolver<{ id: string }, User | null> = async (
-  _,
-  { data: { id } },
-  { query, req },
-  info
+    _,
+    { data: { id } },
+    { query, req },
+    info
 ) => {
-  return (
-    await query.users(
-      {
-        where: {
-          id,
-          register_code: null
-        }
-      },
-      info
-    )
-  )[0];
+    return (
+        await query.users(
+            {
+                where: {
+                    id,
+                    register_code: null
+                }
+            },
+            info
+        )
+    )[0];
 };
 
 export default { searchUsers, getUser };
