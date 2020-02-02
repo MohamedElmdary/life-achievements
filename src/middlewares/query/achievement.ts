135a89764d17b21f57e6bbe34d75e1ce3f15ff08
MohamedElmdary <engm5081@gmail.com>
2020-02-02 12:22:27 +0200

    added userAchievements

--- src/middlewares/query/achievement.ts
+++ src/middlewares/query/achievement.ts
@@ -3,30 +3,42 @@ import { validateAuth, validate, GqlError } from '@utils';
 import { isNumeric } from 'validator';
 
 const feed: Middleware<{ skip: number; limit: number }> = async (
-  resolver,
-  _,
-  { data: { skip, limit } },
-  { req, exists, mutation },
-  info
+    resolver,
+    _,
+    { data: { skip, limit } },
+    { req, exists, mutation },
+    info
 ) => {
-  const userId = await validateAuth(req, exists, mutation);
-  (<any>req).userId = userId;
-  const { valid, errors } = validate(
-    {
-      field: 'skip',
-      msg: 'Skip is required.',
-      valid: isNumeric(skip.toString())
-    },
-    {
-      field: 'limit',
-      msg: 'Limit is required.',
-      valid: isNumeric(limit.toString())
+    const userId = await validateAuth(req, exists, mutation);
+    (<any>req).userId = userId;
+    const { valid, errors } = validate(
+        {
+            field: 'skip',
+            msg: 'Skip is required.',
+            valid: isNumeric(skip.toString())
+        },
+        {
+            field: 'limit',
+            msg: 'Limit is required.',
+            valid: isNumeric(limit.toString())
+        }
+    );
+    if (valid) {
+        return resolver();
     }
-  );
-  if (valid) {
-    return resolver();
-  }
-  throw GqlError(errors);
+    throw GqlError(errors);
 };
 
-export default { feed };
+const userAchievements: Middleware<{}> = async (
+    resolver,
+    _,
+    { data },
+    { req, exists, mutation },
+    info
+) => {
+    const userId = await validateAuth(req, exists, mutation);
+    (<any>req).userId = userId;
+    return resolver();
+};
+
+export default { feed, userAchievements };
--- src/resolvers/query/achievement.ts
+++ src/resolvers/query/achievement.ts
@@ -2,39 +2,48 @@ import { Resolver } from '../resolver.interface';
 import { Achievement } from '@generated';
 
 const feed: Resolver<{ limit: number; skip: number }, Achievement[]> = async (
-  _,
-  { data: { skip, limit } },
-  { req, query },
-  info
-) => {
-  const { userId } = <any>req;
-  console.log(
-    'test',
-    await query.achievements({
-      where: {
-        author: {
-          friends_some: {
-            id: userId
-          }
-        }
-      }
-    })
-  );
-  return await query.achievements(
-    {
-      orderBy: 'created_at_ASC',
-      skip,
-      first: limit,
-      where: {
-        author: {
-          friends_some: {
-            id: userId
-          }
-        },
-        published: true
-      }
-    },
+    _,
+    { data: { skip, limit } },
+    { req, query },
     info
-  );
+) => {
+    const { userId } = <any>req;
+    return await query.achievements(
+        {
+            orderBy: 'created_at_ASC',
+            skip,
+            first: limit,
+            where: {
+                author: {
+                    friends_some: {
+                        id: userId
+                    }
+                },
+                published: true
+            }
+        },
+        info
+    );
 };
-export default { feed };
+
+const userAchievements: Resolver<{ active?: boolean }, Achievement[]> = async (
+    _,
+    { data },
+    { req, query },
+    info
+) => {
+    const { userId } = <any>req;
+    const where: any = {
+        author: {
+            id: userId
+        }
+    };
+    if (data?.active === true || data?.active === false) {
+        where.completed = !data.active;
+    }
+    return await query.achievements({
+        where
+    });
+};
+
+export default { feed, userAchievements };
