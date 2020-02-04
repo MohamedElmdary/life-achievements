93d8ec60762ff9f58d53fa635d1fc2f88597115d
MohamedElmdary <engm5081@gmail.com>
2020-02-04 15:58:09 +0200

    map postgress port from docker & fixed days issue

--- src/resolvers/mutation/achievement.ts
+++ src/resolvers/mutation/achievement.ts
@@ -56,7 +56,7 @@ const completeDay: Resolver<{ achievement_id: string }, Achievement> = async (
     const days: boolean[] = (<any>req).achv.days;
     const timeDiff = new Date().getTime() - new Date(createdAt).getTime();
     const idx = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-    if (idx > 0 && days.length < idx) {
+    if (idx > 0 && days.length >= idx) {
         days[idx - 1] = true;
     }
     return (await mutation.updateAchievement(
@@ -67,7 +67,8 @@ const completeDay: Resolver<{ achievement_id: string }, Achievement> = async (
             data: {
                 days: {
                     set: days
-                }
+                },
+                completed: days.length <= idx
             }
         },
         info
